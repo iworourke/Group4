8f312627706bfbf1896fc1649a5acdb38bd43f18
Alex <alexhuah>
2019-01-23 19:36:08 -0800

    sldgnslkdgls

--- api/bin/app.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import Server from "../server";
-
-new Server().appPromise.then((app: express.Application) => {
-  app.listen(3000, () => {
-    console.log("now listening on port 3000");
-  });
-});
