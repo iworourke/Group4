fee215768d7ac521109888436bbf6b4d876489cc
Ian O'Rourke <iworourke@gmail.com>
2019-01-30 16:50:30 -0800

    Add files via upload

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,6 +5,7 @@
         <div class="navbar-start">
           <router-link class="navbar-item is-tab" to="/" exact-active-class="is-active">Home</router-link> 
           <router-link class="navbar-item is-tab" to="/about" exact-active-class="is-active">About</router-link>
+          <router-link class="navbar-item is-tab" to="/todos" exact-active-class="is-active">ToDos</router-link>
         </div>
         <div class="navbar-end">
           <div class="navbar-item">
@@ -21,19 +22,25 @@
       </div>
     </div>
     <router-view/>
-    <Signup v-bind:is-showing="showSignup" v-on:success="successSignup()" v-on:cancel="cancelSignup()"/>
-    <Login v-bind:is-showing="showLogin" v-on:success="successLogin()" v-on:cancel="cancelLogin()"/>
+    <Modal v-bind:is-showing="showSignup" title="Signup" v-on:success="successSignup()" v-on:cancel="cancelSignup()">
+      <Signup/>
+    </Modal>
+    <Modal v-bind:is-showing="showLogin" title="Login" v-on:success="successLogin()" v-on:cancel="cancelLogin()">
+      <Login/>
+    </Modal>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import Modal from "@/components/Modal.vue";
 import Signup from "@/components/Signup.vue";
 import Login from "@/components/Login.vue";
 
 @Component({
   components: {
+    Modal,
     Signup,
     Login
   }
