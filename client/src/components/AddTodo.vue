e3a9e4f5d32c24b6dbd3955f8a534b8bdcc20e34
Ian O'Rourke <iworourke@gmail.com>
2019-01-30 16:49:50 -0800

    Add files via upload

---
+++ client/src/components/AddTodo.vue
@@ -0,0 +1,33 @@
+<template>
+  <form  v-on:submit.prevent="onSubmit">
+    <div class="field">
+      <label class="label">Task Name</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="task name" v-bind="addtodo.taskName"/>
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Task Status</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="task status" v-bind="addtodo.taskStatus"/>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class AddTodo extends Vue {
+  addtodo: AddTodoForm = {
+    taskName: "",
+    taskStatus: ""
+  };
+}
+
+export interface AddTodoForm {
+  taskName: string;
+  taskStatus: string;
+}
+</script>
